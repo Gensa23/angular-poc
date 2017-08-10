52ac0c73987a2db19f22e33b51a6ffc23044c53d
Abdullah <z24abdullah@gmail.com>
2017-08-10 10:41:26 +0300

    Edit profile & submit feature.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './user.service';
+import {User} from './user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [UserService]
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  constructor(private userService: UserService) {
+  }
+
+  editable = false;
+
+  newUser: User;
+  user: User;
+
+  ngOnInit(): void {
+      this.getUserProfile();
+  }
+
+  editClickButton(): void {
+      this.editable = true;
+  }
+
+  getUserProfile(): void {
+      this.userService.getUserProfile().subscribe(user => this.user = user);
+    }
+  submitProfile(name: string, age: number): void {
+      console.log(name + age);
+      this.userService.updateUserProfile(new User(name, age)).then(user => this.user = user);
+      this.editable = false;
+
+  }
 }

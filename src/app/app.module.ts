83442568b83045e70338dcebc9b11bb402b3271a
Mohammed Alshehri <fcm2009@gmail.com>
2017-08-16 09:32:44 +0300

    done with routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileEditorComponent } from './profile-editor/profile-editor.component';
+import { ProfileViewerComponent } from './profile-viewer/profile-viewer.component';
+
+const routes: Routes = [
+  { path: 'profile', component: ProfileComponent },
+  { path: 'profile/view', component: ProfileViewerComponent },
+  { path: 'profile/edit', component: ProfileEditorComponent },
+  { path: '', component: HomeComponent }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent
+    ProfileComponent,
+    HomeComponent,
+    ProfileEditorComponent,
+    ProfileViewerComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
